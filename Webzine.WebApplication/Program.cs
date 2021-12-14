de6ec6b73f512688d6d83abaefa60b462ddaa701
Aurélien Bissey <Aurelien.Bissey@diiage.org>
2021-12-14 14:18:46 +0100

    add package Runtime

--- Webzine.WebApplication/Program.cs
+++ Webzine.WebApplication/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 app.Run();
