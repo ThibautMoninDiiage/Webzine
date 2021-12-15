2a4c9c4d7a01d20085d68314e25ad9c231fd22a6
Aurélien Bissey <Aurelien.Bissey@diiage.org>
2021-12-15 16:55:36 +0100

    Add Contact page

---
+++ Webzine.WebApplication/Controllers/Contact/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webzine.WebApplication.Controllers.Contact
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
