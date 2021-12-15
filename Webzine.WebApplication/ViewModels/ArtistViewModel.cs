5fb793af3751ef99805ba1cc6a432a07fdd19356
Quentin Bourdelot <quentin.bourdelot@diiage.org>
2021-12-15 15:28:21 +0100

    title page

---
+++ Webzine.WebApplication/ViewModels/ArtistViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Webzine.Entity
+{
+    public class ArtistViewModel
+    {
+        public int IdArtist { get; set; }
+        public string Name { get; set; }
+        //*public Title Title { get; set; }
+
+    }
+}
+
---
+++ Webzine.WebApplication/ViewModels/TitleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Webzine.Entity
+{
+    public class TitleViewModel
+    {
+        public int IdTitle { get; set; }
+        public string Name { get; set; }
+
+    }
+}
