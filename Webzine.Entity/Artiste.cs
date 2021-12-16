53d726d668fef5ccec652dfa256173b097602fd5
Thibaut Monin <Thibaut.Monin@diiage.org>
2021-12-16 11:50:24 +0100

    Tests are running 100%

--- Webzine.Entity/Artiste.cs
+++ Webzine.Entity/Artiste.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace Webzine.Entity
 {
     public class Artiste
     {
+        [Key]
         public int IdArtiste { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        [Display(Name = "Nom de l'artiste")]
         public string Nom { get; set; }
+
+        [Required]
         public string Biographie { get; set; }
+
+        [Required]
         public List<Titre> Titres { get; set; }
     }
 }
