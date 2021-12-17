77c21f332320fd26c9309e0c9b91579f56e6cbc5
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-17 14:16:06 +0100

    Modify calling method name

--- Webzine.Entity/Factory/TitleFactory.cs
+++ Webzine.Entity/Factory/TitleFactory.cs
@@ -20,7 +20,7 @@ namespace Webzine.Entity.Factory
                 Libelle = "Papillon",
                 NbLikes = 37651,
                 NbLectures = 99328,
-                TitresStyles = StyleFactory.CreateStyles(3).ToList(),
+                TitresStyles = StyleFactory.GetStyles(3).ToList(),
                 UrlEcoute = "https://www.youtube.com/watch?v=diSQuucDoBA",
                 UrlJaquette = "https://static.fnac-static.com/multimedia/Images/FR/NR/08/c5/c2/12764424/1540-1/tsp20201019142053/Survie.jpg", Chronique = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             };
@@ -41,7 +41,7 @@ namespace Webzine.Entity.Factory
                 Libelle = "Trois sabres zéro",
                 NbLikes = 1200,
                 NbLectures = 3070,
-                TitresStyles = StyleFactory.CreateStyles(1).ToList(),
+                TitresStyles = StyleFactory.GetStyles(1).ToList(),
                 UrlEcoute = "https://www.youtube.com/watch?v=vMM_Es3tB0U",
                 UrlJaquette = "https://images.genius.com/7947a8e0e74c950a435d781098bf9bab.1000x1000x1.jpg",
                 Chronique = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -63,7 +63,7 @@ namespace Webzine.Entity.Factory
                 Libelle = "Macarena",
                 NbLikes = 17901,
                 NbLectures = 76964,
-                TitresStyles = StyleFactory.CreateStyles(5).ToList(),
+                TitresStyles = StyleFactory.GetStyles(5).ToList(),
                 UrlEcoute = "https://www.youtube.com/watch?v=GGhKPm18E48",
                 UrlJaquette = "https://m.media-amazon.com/images/I/61yWfxvZfGL._SL1400_.jpg",
                 Chronique = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -74,6 +74,9 @@ namespace Webzine.Entity.Factory
 
             return titles;
         }
+
+
+
     }
 }
 
