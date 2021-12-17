251777f8fe91886c483b1beb3cfa686cf2031d67
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-17 15:48:42 +0100

    Add artist page

--- Webzine.WebApplication/Controllers/Artist/ArtistController.cs
+++ Webzine.WebApplication/Controllers/Artist/ArtistController.cs
@@ -8,19 +8,30 @@ namespace Webzine.WebApplication.Controllers
 {
     public class ArtistController : Controller
     {
-        public Artiste Artist => ArtistFactory.GetArtiste();
+        public Artiste Artist => ArtistFactory.GetArtist();
         public IEnumerable<Style> Styles => StyleFactory.CreateStyles(); 
 
         public IActionResult Index()
         {
+           
+
             var model = new ArtistViewModel
             {
                 Artist = this.Artist
             };
 
+
+            //foreach (var title in model.Artist.Titres)
+            //{
+            //    TimeSpan time = TimeSpan.FromSeconds(seconds);
+            //    title.Duree = time.ToString(@"mm\:ss");
+            //}
+
             ViewBag.Styles = this.Styles.ToList();
             return this.View(model);
         }
     }
+
+
 }
 
