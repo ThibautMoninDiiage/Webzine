d50eae8964e1124e4c8c37c329e5594ca446514b
Quentin Bourdelot <quentin.bourdelot@diiage.org>
2021-12-17 11:32:13 +0100

    Created search pages and started reworking title design

---
+++ Webzine.WebApplication/Controllers/Search/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webzine.WebApplication.Controllers.Contact
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
