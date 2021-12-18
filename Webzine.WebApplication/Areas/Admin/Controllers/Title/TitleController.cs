79bf0dbdf03fa111fa81bd6d69bd29e053d42675
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-18 19:55:53 +0100

    Create title index and controller

---
+++ Webzine.WebApplication/Areas/Admin/Controllers/Title/TitleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Webzine.Entity.Factory;
+using Webzine.WebApplication.Areas.Admin.ViewModels;
+
+namespace Webzine.WebApplication.Areas.Admin.Controllers.Title
+{
+    [Area("Admin")]
+    public class TitleController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var model = new TitleViewModel
+            {
+                Titres = TitleFactory.CreateTitles()
+            };
+            return this.View(model);
+        }
+    }
+}
+
