e63fb8ec70e21563670f263eedb51b6138e90702
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-18 19:41:43 +0100

    Add admin styles management

---
+++ Webzine.WebApplication/Areas/Admin/Controllers/Style/StyleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webzine.WebApplication.Areas.Admin.Controllers.Style
+{
+    [Area("Admin")]
+    public class StyleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View("Create");
+        }
+
+        public IActionResult Delete()
+        {
+
+            return this.View("Delete");
+        }
+    }
+}
+
