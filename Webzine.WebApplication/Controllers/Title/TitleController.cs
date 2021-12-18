db0c8b6b390f7175ffcd72b68f4f8cde8a9beb09
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-18 17:02:37 +0100

    Edit the viewModel

--- Webzine.WebApplication/Controllers/Title/TitleController.cs
+++ Webzine.WebApplication/Controllers/Title/TitleController.cs
@@ -1,28 +1,18 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Webzine.Entity;
+using Webzine.Entity.Factory;
 using Webzine.WebApplication.ViewModels;
 
 namespace Webzine.WebApplication.Controllers.Title
 {
     public class TitleController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int IdTitre)
         {
             var model = new TitleViewModel()
             {
-                IdTitle = 1,
-                Name = "Papillon",
-                Chronique = "Ceci est la chronique du titre Papillon de l'artiste Zola",
-                DateCreation = DateTime.Now,
-                NbLikes = 139821,
-                TitresStyles = new List<Style>() {
-                    new Style() { IdStyle = 1, Libelle = "Jazz" },
-                    new Style() { IdStyle = 2, Libelle = "Rap" }
-                },
-                Libelle = "Papillon",
-                Artiste = new Artiste() { Nom = "Zola" },
-                UrlJaquette = "https://static.fnac-static.com/multimedia/Images/FR/NR/08/c5/c2/12764424/1540-1/tsp20201019142053/Survie.jpg"
+                Titre = TitleFactory.CreateTitles().Where(t => t.IdTitre == IdTitre).FirstOrDefault()
             };
 
             return this.View(model);
--- Webzine.WebApplication/ViewModels/TitleViewModel.cs
+++ Webzine.WebApplication/ViewModels/TitleViewModel.cs
@@ -4,21 +4,6 @@ namespace Webzine.WebApplication.ViewModels
 {
     public class TitleViewModel : BaseViewModel
     {
-        public int IdTitle { get; set; }
-        public string Name { get; set; }
-        public int IdArtiste { get; set; }
-        public Artiste Artiste { get; set; }
-        public string Libelle { get; set; }
-        public string Chronique { get; set; }
-        public DateTime DateCreation { get; set; }
-        public int Duree { get; set; }
-        public DateTime DateSortie { get; set; }
-        public string UrlJaquette { get; set; }
-        public string UrlEcoute { get; set; }
-        public int NbLectures { get; set; }
-        public int NbLikes { get; set; }
-        public string Album { get; set; }
-        public List<Style> TitresStyles { get; set; }
-        public List<Commentaire> Commentaires { get; set; }
+        public Titre Titre { get; set; }
     }
 }
