9c23f65209658cc4970e271e45e9e2f6a44ff764
Thibaut Monin <Thibaut.Monin@diiage.org>
2021-12-19 18:19:40 +0100

    Bugfix partie 2, redirection cover, substring, conditions last released

--- Webzine.WebApplication/Controllers/Home/HomeController.cs
+++ Webzine.WebApplication/Controllers/Home/HomeController.cs
@@ -9,14 +9,25 @@ namespace Webzine.WebApplication.Controllers
     public class HomeController : Controller
     {
         public IEnumerable<Titre> MostPopularTitles => TitleFactory.CreateTitles().OrderByDescending(t => t.NbLikes).Take(3);
-        public IEnumerable<Titre> LastReleasedTitles => TitleFactory.CreateTitles().OrderByDescending(t => t.DateCreation).Take(3);
+        public IEnumerable<Titre> AllTitles => TitleFactory.CreateTitles();
+        public IEnumerable<Titre> OrderedTitles { get; set; }
 
-        public IActionResult Index()
+        public IActionResult Index(bool isLastReleased = true)
         {
+
+            if (isLastReleased)
+            {
+                OrderedTitles = AllTitles.OrderByDescending(t => t.DateCreation).Take(3);
+            } else
+            {
+                OrderedTitles = AllTitles.OrderBy(t => t.DateCreation).Take(3);
+            }
+
             var model = new HomeViewModel
             {
-                MostPopularTitles = this.MostPopularTitles.ToList(),
-                LastReleasedTitles = this.LastReleasedTitles.ToList()
+                MostPopularTitles = this.MostPopularTitles,
+                OrderedTitles = this.OrderedTitles,
+                IsLastReleased = isLastReleased
             };
 
             return this.View(model);
