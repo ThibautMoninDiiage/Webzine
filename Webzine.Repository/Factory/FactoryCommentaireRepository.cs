ade3de175e523157d9f191749759f8bdb48e93c8
ThomasBernardPageUp <tbernard@pageup.fr>
2022-01-04 14:36:11 +0100

    add commentaire factory repo

---
+++ Webzine.Repository/Factory/FactoryCommentaireRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Webzine.Entity;
+using Webzine.Repository.Contracts;
+
+namespace Webzine.Repository.Factory
+{
+    public class FactoryCommentaireRepository : ICommentaireRepository
+    {
+        public FactoryCommentaireRepository()
+        {
+        }
+
+        public void Add(Commentaire commentaire)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Commentaire commentaire)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Commentaire Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Commentaire> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
