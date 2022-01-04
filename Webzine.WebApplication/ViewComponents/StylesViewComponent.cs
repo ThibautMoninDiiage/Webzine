40a69b2960629105e67ef12b5870b4cf558832ae
Quentin Bourdelot <quentin.bourdelot@diiage.org>
2022-01-04 15:20:32 +0100

    Add ViewComponent

---
+++ Webzine.WebApplication/ViewComponents/StylesViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webzine.Entity;
+using Webzine.Entity.Factory;
+
+namespace Webzine.WebApplication.ViewComponents
+{
+    public class StylesViewComponent : ViewComponent
+    {
+        public StylesViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(StyleFactory.CreateStyles());
+        }
+
+    }
+}
