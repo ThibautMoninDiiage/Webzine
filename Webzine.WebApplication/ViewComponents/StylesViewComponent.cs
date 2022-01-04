86ffc9b40e12fe23f51435fb1834d145ce7cefaf
ThomasBernardPageUp <tbernard@pageup.fr>
2022-01-04 16:12:31 +0100

    Fix source data

--- Webzine.WebApplication/ViewComponents/StylesViewComponent.cs
+++ Webzine.WebApplication/ViewComponents/StylesViewComponent.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Webzine.Entity;
 using Webzine.Entity.Factory;
+using Webzine.Repository.Contracts;
 
 namespace Webzine.WebApplication.ViewComponents
 {
     public class StylesViewComponent : ViewComponent
     {
-        public StylesViewComponent()
+        private IStyleRepository _styleRepository;
+
+        public StylesViewComponent(IStyleRepository styleRepository)
         {
+            _styleRepository = styleRepository;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View(StyleFactory.CreateStyles());
+            return View(_styleRepository.FindAll());
         }
 
     }
