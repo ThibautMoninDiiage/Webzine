bf4f51a95fd2c9fd29247502347abbbd66b98765
Thibaut Monin <Thibaut.Monin@diiage.org>
2022-01-06 11:19:35 +0100

    Webzine

--- Webzine.EntitiesContext/WebzineDbContext.cs
+++ Webzine.EntitiesContext/WebzineDbContext.cs
@@ -37,14 +37,56 @@ namespace Webzine.EntitiesContext
         {
             modelBuilder.Entity<Artiste>(entity =>
             {
+                // Défini le nom de la table dans la base de données
                 entity.ToTable("ARTISTE");
+                // Défini le nom des champs dans la base de données
                 entity.Property(e => e.IdArtiste).HasColumnName("Id");
                 entity.Property(e => e.Nom).HasColumnName("Nom");
                 entity.Property(e => e.Biographie).HasColumnName("Biographie");
-                // entity.Property(e => e.Titres).HasColumnName("Titre");
+                entity.Property(e => e.Titres).HasColumnName("Titre");
                 entity.Property(e => e.DateNaissance).HasColumnName("DateNaissance");
                 entity.Property(e => e.UrlSite).HasColumnName("UrlSite");
             });
+
+            modelBuilder.Entity<Commentaire>(entity =>
+            {
+                entity.ToTable("COMMENTAIRE");
+                entity.Property(e => e.IdCommentaire).HasColumnName("Id");
+                entity.Property(e => e.Contenu).HasColumnName("Contenu");
+                entity.Property(e => e.Auteur).HasColumnName("Auteur");
+                entity.Property(e => e.DateCreation).HasColumnName("DateCreation");
+                entity.Property(e => e.IdTitre).HasColumnName("IdTitre");
+                entity.Property(e => e.Titre).HasColumnName("Titre");
+            });
+
+            modelBuilder.Entity<Style>(entity =>
+            {
+                entity.ToTable("STYLE");
+                entity.Property(e => e.IdStyle).HasColumnName("Id");
+                entity.Property(e => e.Libelle).HasColumnName("Libelle");
+                entity.Property(e => e.TitresStyles).HasColumnName("TitresStyles");
+            });
+
+            modelBuilder.Entity<Titre>(entity =>
+            {
+                entity.ToTable("TITRE");
+                entity.Property(e => e.IdTitre).HasColumnName("Id");
+                entity.Property(e => e.IdArtiste).HasColumnName("IdArtiste");
+                entity.Property(e => e.Artiste).HasColumnName("Artiste");
+                entity.Property(e => e.Libelle).HasColumnName("Libelle");
+                entity.Property(e => e.Chronique).HasColumnName("Chronique");
+                entity.Property(e => e.DateCreation).HasColumnName("DateCreation");
+                entity.Property(e => e.Duree).HasColumnName("Duree");
+                entity.Property(e => e.DateSortie).HasColumnName("DateSortie");
+                entity.Property(e => e.UrlJaquette).HasColumnName("UrlJaquette");
+                entity.Property(e => e.UrlEcoute).HasColumnName("UrlEcoute");
+                entity.Property(e => e.NbLectures).HasColumnName("NbLectures");
+                entity.Property(e => e.NbLikes).HasColumnName("NbLikes");
+                entity.Property(e => e.Album).HasColumnName("Album");
+                entity.Property(e => e.TitresStyles).HasColumnName("TitresStyles");
+                entity.Property(e => e.Commentaires).HasColumnName("Commentaires");
+            });
+
         }
 
     }
