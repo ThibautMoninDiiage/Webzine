5e555ac73462c80d486f78bdf46ea8f7f91bfb2d
Thibaut Monin <Thibaut.Monin@diiage.org>
2022-01-07 14:47:53 +0100

    Increment Like + Vues

--- Webzine.WebApplication/Controllers/Titre/TitreController.cs
+++ Webzine.WebApplication/Controllers/Titre/TitreController.cs
@@ -18,13 +18,25 @@ namespace Webzine.WebApplication.Controllers.Title
         public IActionResult Index(int idTitre)
         {
             _logger.LogInformation("Accès à la page titre.");
+            var titre = _titreRepository.Find(idTitre);
+            _titreRepository.IncrementNbLectures(titre);
 
             var model = new TitleViewModel()
             {
                 Titre = _titreRepository.Find(idTitre)
             };
 
-            return this.View(model);
+            return this.View("Index", model);
         }
+
+        [HttpPost]
+        public IActionResult Liker(int idTitre)
+        {
+            var titre = _titreRepository.Find(idTitre);
+            _titreRepository.IncrementNbLikes(titre);
+
+            return this.Index(idTitre);
+        }
+
     }
 }
