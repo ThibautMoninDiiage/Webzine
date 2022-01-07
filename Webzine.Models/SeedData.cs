177c6a386ad4e7f7ee5447d37b8e945d29116dad
Thibaut Monin <Thibaut.Monin@diiage.org>
2022-01-07 16:41:28 +0100

    Seeding des data

---
+++ Webzine.Models/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Webzine.EntitiesContext;
+using Webzine.Entity.Factory;
+
+namespace Webzine.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WebzineDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<WebzineDbContext>>()))
+            {
+                if (context.Artistes.Any())
+                {
+                    return;
+                } else
+                {
+                    context.Artistes.AddRange(ArtistFactory.CreateArtists());
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
