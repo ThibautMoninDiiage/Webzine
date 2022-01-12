e7a802eafd245298eac5124d293ee64d3dfaf59e
ThomasBernardPageUp <tbernard@pageup.fr>
2022-01-12 10:23:16 +0100

    Create RechercheService

---
+++ Webzine.Business.Contracts/IRechercheService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Webzine.Entity;
+
+namespace Webzine.Business.Contracts
+{
+    public interface IRechercheService
+    {
+        /// <summary>
+        /// Retourne une liste de titres qui correspond au mot clé passé en paramètre
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        IEnumerable<Titre> RechercherTitre(string keyword = "");
+
+        /// <summary>
+        /// Retourne une liste d'artiste qui correspond au mot clé passé en paramètre
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        IEnumerable<Artiste> RechercherArtiste(string keyword = "");
+    }
+}
+
