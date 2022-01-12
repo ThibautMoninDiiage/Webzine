f070581464154b875815174a5c6547e77136f7a0
ThomasBernardPageUp <tbernard@pageup.fr>
2022-01-12 15:11:59 +0100

    create style dto

--- Webzine.Entity/DTO/TitreDTO.cs
+++ Webzine.Entity/DTO/TitreDTO.cs
@@ -1,33 +1,71 @@
 ﻿using System;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Webzine.Entity.Interfaces;
 
 namespace Webzine.Entity.DTO
 {
-    public class TitreDTO
+    public class TitreDTO : ITitre
     {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
+        [JsonProperty("id")]
+        public int IdTitre { get; set; }
 
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
+        public int IdArtiste { get; set; }
 
-        [JsonPropertyName("link")]
-        public string Link { get; set; }
+        [JsonProperty("artist")]
+        public Artiste Artiste { get; set; }
 
-        [JsonPropertyName("duration")]
-        public int Duration { get; set; }
+        [JsonProperty("title")]
+        public string Libelle { get; set; }
 
-        [JsonPropertyName("rank")]
-        public int Rank { get; set; }
+        public string Chronique { get; set; }
 
-        [JsonPropertyName("preview")]
-        public string Preview { get; set; }
+        public DateTime DateCreation { get; set; }
 
-        [JsonPropertyName("artist")]
-        public ArtistDTO Artist { get; set; }
+        [JsonProperty("duration")]
+        public int Duree { get; set; }
 
-        [JsonPropertyName("album")]
-        public AlbumDTO Album { get; set; }
+        public DateTime DateSortie { get; set; }
+
+        public string UrlJaquette { get; set; }
+
+        [JsonProperty("preview")]
+        public string UrlEcoute { get; set; }
+
+        [JsonProperty("rank")]
+        public int NbLectures { get; set; }
+
+        public int NbLikes { get; set; }
+
+        [JsonProperty("album")]
+        public AlbumDTO AlbumDTO { get; set; }
+
+        public string Album { get; set; }
+
+        public List<Style> TitresStyles { get; set; }
+        public List<Commentaire> Commentaires { get; set; }
+
+
+
+        public TitreDTO(ITitre titre)
+        {
+            this.IdArtiste = titre.IdArtiste;
+            this.IdArtiste = titre.IdArtiste;
+            this.Artiste = titre.Artiste;
+            this.Libelle = titre.Libelle;
+            this.Chronique = titre.Chronique;
+            this.DateCreation = titre.DateCreation;
+            this.Duree = titre.Duree;
+            this.DateSortie = titre.DateSortie;
+            this.UrlJaquette = titre.UrlJaquette;
+            this.UrlEcoute = titre.UrlEcoute;
+            this.NbLectures = titre.NbLectures;
+            this.NbLikes = titre.NbLikes;
+            this.Album = titre.Album;
+            this.TitresStyles = titre.TitresStyles;
+            this.Commentaires = titre.Commentaires;
+        }
     }
+
+    
 }
 
