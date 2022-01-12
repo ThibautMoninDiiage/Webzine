18bec96c5d0a141822728db1c58071d3ad8ef4db
ThomasBernardPageUp <tbernard@pageup.fr>
2022-01-12 14:17:17 +0100

    get artist and albums

--- Webzine.Models/DeezerSeedData.cs
+++ Webzine.Models/DeezerSeedData.cs
@@ -4,49 +4,62 @@ using Newtonsoft.Json;
 using Webzine.EntitiesContext;
 using Webzine.Entity;
 using Webzine.Entity.DTO;
+using Webzine.Repository.Contracts;
 
 namespace Webzine.Models
 {
     public class DeezerSeedData
     {
+
+
         public async static void Initialize(IServiceProvider serviceProvider)
         {
+            
             using (var context = new WebzineDbContext(serviceProvider.GetRequiredService<DbContextOptions<WebzineDbContext>>()))
             {
-                if (context.Artistes.Any())
-                {
-                    return;
-                }
-                else
-                {
-                    context.Artistes.AddRange(await SeedArtiste());
-                }
-
-                if (context.Styles.Any())
-                {
-                    return;
-                }
-                else
-                {
-                    context.Styles.AddRange(await SeedStyle());
-                }
-
                 if (context.Titres.Any())
                 {
                     return;
                 }
                 else
                 {
-                    var titres = await SeedPlaylist();
-                    foreach (var titre in titres)
+
+                    var resultTitres = await SeedTitre();
+                    IEnumerable<ArtistDTO> artistes;
+                    artistes = resultTitres.Select(t => t.Artist).DistinctBy(a => a.Name);
+
+
+                    foreach (var artiste in artistes)
                     {
-                        // Je récupère la tracklist de la playlist (tous les sons)
-                        var tracklist = titre.Tracklist;
-                        var endpoint = tracklist.Split("m/");
-                        // Je dois faire un call sur cette API pour récupérer toutes les informations d'un titre
-                        // (Id, title, duration, etc etc, artist, album)
-                        var call = await HttpCall<DeezerRequest>("playlist/757807/tracks");
-                        context.Titres.Add(titre);
+                        context.Artistes.Add(new Artiste
+                        {
+                            IdArtiste = artiste.Id,
+                            Biographie = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                            Nom = artiste.Name,
+                            UrlSite = artiste.Link
+                        });
+                    }
+
+                    context.SaveChanges();
+
+                    foreach (var titre in resultTitres)
+                    {
+                        context.Titres.AddRange(new Titre
+                        {
+                            IdTitre = titre.Id,
+                            Album = titre.Album.Title,
+                            Artiste = context.Artistes.Find(titre.Artist.Id),
+                            Duree = titre.Duration,
+                            UrlJaquette = titre.Album.Cover,
+                            DateSortie = DateTime.Now,
+                            Libelle = titre.Title,
+                            NbLikes = 0,
+                            NbLectures = titre.Rank,
+                            Chronique = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                            IdArtiste = titre.Artist.Id,
+                            DateCreation = DateTime.Now,
+                            UrlEcoute = titre.Preview
+                        });
                     }
                 }
 
@@ -55,50 +68,30 @@ namespace Webzine.Models
             }
         }
 
-        public async static Task<T> HttpCall<T>(string endpoint)
+        /// <summary>
+        /// Cette méthode envoie une requète http et retourne le résultat déserialisé
+        /// </summary>
+        /// <typeparam name="T">Une classe</typeparam>
+        /// <param name="url">Une url qui retourne du JSON</param>
+        /// <returns>Une instance d'objet</returns>
+        public async static Task<T> HttpCall<T>(string url)
         {
-            var baseURI = "https://api.deezer.com/";
             // Instance de client HTTP.
             var httpClient = new HttpClient();
             // Définition de l'URL de base.
-            httpClient.BaseAddress = new Uri(baseURI);
-            HttpResponseMessage response = httpClient.GetAsync(endpoint).Result;
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
             response.EnsureSuccessStatusCode();
             var entity = response.Content.ReadAsStringAsync().Result;
-            T result = JsonConvert.DeserializeObject<T>(entity);
-            return result;
-        }
-        
-        public async static Task<Artiste> SeedArtiste()
-        {
-            var artiste = await HttpCall<Artiste>("artist/zola");
-            artiste.Biographie = "La biographie de cet artiste n'a pas pu être récupérée.";
-            return artiste;
+            return JsonConvert.DeserializeObject<T>(entity);
         }
 
-        public async static Task<Titre> SeedTitre()
+        public async static Task<IEnumerable<TitreDTO>> SeedTitre()
         {
-            var titre = await HttpCall<Titre>("track/3135556");
-            return titre;
-        }
+            var deezerRequest = await HttpCall<DeezerRequestRootDTO>("https://api.deezer.com/playlist/1109890291/tracks");
 
-        public async static Task<Style> SeedStyle()
-        {
-            var style = await HttpCall<Style>("genre/5");
-            return style;
-        }
 
-        public async static Task<List<Titre>> SeedPlaylist()
-        {
-            var playlist = await HttpCall<DeezerRequest>("user/2529/playlists");
-            return playlist.Data;
+            return deezerRequest.Data;
         }
-
-        //public async static Task<List<TrackDTO>> SeedTracklist()
-        //{
-        //    var tracklist = await HttpCall<TrackDTO>("playlist/757807/tracks");
-        //    return tracklist;
-        //}
 
     }
 }
