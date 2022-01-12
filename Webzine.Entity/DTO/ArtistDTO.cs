333513f06927ea925e357b93955775071b9edde0
ThomasBernardPageUp <tbernard@pageup.fr>
2022-01-12 14:16:01 +0100

    create dto

---
+++ Webzine.Entity/DTO/ArtistDTO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Webzine.Entity.DTO
+{
+    public class ArtistDTO
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("link")]
+        public string Link { get; set; }
+
+        [JsonPropertyName("picture")]
+        public string Picture { get; set; }
+
+        [JsonPropertyName("picture_small")]
+        public string PictureSmall { get; set; }
+
+        [JsonPropertyName("picture_medium")]
+        public string PictureMedium { get; set; }
+
+        [JsonPropertyName("picture_big")]
+        public string PictureBig { get; set; }
+
+        [JsonPropertyName("picture_xl")]
+        public string PictureXl { get; set; }
+
+        [JsonPropertyName("tracklist")]
+        public string Tracklist { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
+
---
+++ Webzine.Entity/DTO/TitreDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Webzine.Entity.DTO
+{
+    public class TitreDTO
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("link")]
+        public string Link { get; set; }
+
+        [JsonPropertyName("duration")]
+        public int Duration { get; set; }
+
+        [JsonPropertyName("rank")]
+        public int Rank { get; set; }
+
+        [JsonPropertyName("preview")]
+        public string Preview { get; set; }
+
+        [JsonPropertyName("artist")]
+        public ArtistDTO Artist { get; set; }
+
+        [JsonPropertyName("album")]
+        public AlbumDTO Album { get; set; }
+    }
+}
+
