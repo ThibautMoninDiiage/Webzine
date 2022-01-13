df52c3065272a22439fa1ab456cf0842b1e18ad5
Thomas BERNARD <thomas.bernard@diiage.org>
2022-01-13 11:41:51 +0100

    create different services

---
+++ Webzine.Business.Contracts/IPictureService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webzine.Business.Contracts
+{
+    public interface IPictureService
+    {
+        /// <summary>
+        /// Enregistre une image en local dans un dossier du projet
+        /// </summary>
+        /// <param name="urlPicture">Url complet de l'image</param>
+        void SavePicture(string urlPicture, string filename);
+    }
+}
---
+++ Webzine.Business/PictureService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Webzine.Business.Contracts;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Webzine.Business
+{
+    public class PictureService : IPictureService
+    {
+        public void SavePicture(string urlPicture, string filename)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(urlPicture);
+
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    using (var yourImage = Image.FromStream(mem))
+                    {
+                        yourImage.Save("Image2.png", ImageFormat.Jpeg);
+                    }
+                }
+            }
+        }
+    }
+}
