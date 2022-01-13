8d49bc370ecb60f849fab3186d770f336d4dc168
Thomas BERNARD <thomas.bernard@diiage.org>
2022-01-13 14:45:31 +0100

    edit json service

--- Webzine.Business.Contracts/IJsonService.cs
+++ Webzine.Business.Contracts/IJsonService.cs
@@ -9,7 +9,7 @@ namespace Webzine.Business.Contracts
 {
     public interface IJsonService
     {
-        void WriteJsonFile<T>(string path, T param);
-        T ReadJsonFile<T>(string fileName);
+        void WriteJsonFile<T>(string filename, T param);
+        T ReadJsonFile<T>(string filename);
     }
 }
