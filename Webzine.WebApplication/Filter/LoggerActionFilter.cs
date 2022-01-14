e9ae1c2e1a9b010963fed3eb29459d54203c7988
Quentin Bourdelot <quentin.bourdelot@gmail.com>
2022-01-14 14:23:15 +0100

    Creation Exception Filter

---
+++ Webzine.WebApplication/Filter/LoggerActionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Webzine.WebApplication.Filter
+{
+    public class LoggerActionFilter : IActionFilter, IExceptionFilter
+    {
+        private readonly ILogger<LoggerActionFilter> _logger;
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Logguer les informations en sortie de l'action
+            Console.WriteLine("Sortie action");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Logguer les informations à l'entrée de l'action
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            // Logguer les exceptions et rediriger vers une page d'erreur
+            context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+            {
+                controller = "Erreur",
+                action = "Erreur"
+            }));
+        }
+    }
+}
